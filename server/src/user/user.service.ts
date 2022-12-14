40f220675765119e3fad0c5a37be56599a023c65
imMatheus <matheusmendesbarata03@gmail.com>
2022-12-14 21:37:34 +0100

    changed .gitignore

--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-
+import { PrismaService } from '../prisma.service';
 @Injectable()
 export class UserService {
+  constructor(private prisma: PrismaService) {}
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
 
   findAll() {
-    return `This action returns all user`;
+    return this.prisma.user.findMany();
   }
 
   findOne(id: number) {
